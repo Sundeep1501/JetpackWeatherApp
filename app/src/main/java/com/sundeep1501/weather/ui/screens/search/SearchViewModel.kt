8139348f9da08ea13095aaa50dea2b04e1a6357e
Sundeep Penkay <sundeep1501@gmail.com>
2026-09-12 12:58:54 -0700

    ideomatic refactoring and converting cold to hot flow cleanly

--- app/src/main/java/com/sundeep1501/weather/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/sundeep1501/weather/ui/screens/search/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.sundeep1501.weather.ui.screens.search
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sundeep1501.weather.data.WeatherRepository
@@ -8,13 +7,15 @@ import com.sundeep1501.weather.data.models.City
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -22,57 +23,37 @@ import kotlin.time.Duration.Companion.milliseconds
 class SearchViewModel @Inject constructor(private val weatherRepository: WeatherRepository) :
     ViewModel() {
 
-    private val _searchUiState = MutableStateFlow<SearchUiState>(SearchUiState.Empty)
-    val searchUiState = _searchUiState.asStateFlow()
-
     private val _searchQuery = MutableStateFlow("")
     val searchQuery = _searchQuery.asStateFlow()
 
-    init {
-        viewModelScope.launch {
-            _searchQuery.map { it.trim() }.filter { query ->
-                if (query.isBlank()) {
-                    _searchUiState.value = SearchUiState.Empty
-                    false
-                } else {
-                    query.length >= 3
-                }
-            }.debounce(500L.milliseconds).distinctUntilChanged().collectLatest { validQuery ->
-                search(validQuery)
-            }
-        }
-    }
+    val searchUiState: StateFlow<SearchUiState> =
+        _searchQuery
+            .map { it.trim() }
+            .debounce(500.milliseconds)
+            .distinctUntilChanged()
+            .flatMapLatest { query ->
+                flow {
+                    if (query.length < 3) {
+                        emit(SearchUiState.Empty)
+                        return@flow
+                    }
 
-    private suspend fun search(validQuery: String) {
-        try {
-            val cities = weatherRepository.getCities(validQuery)
-            handleResult(cities)
-        } catch (cancelEx: CancellationException) {
-            throw cancelEx
-        } catch (ex: Exception) {
-            handleException(ex)
-        }
-    }
+                    emit(SearchUiState.Loading)
+                    try {
+                        emit(SearchUiState.Success(weatherRepository.getCities(query)))
+                    } catch (ex: Exception) {
+                        if (ex is CancellationException) throw ex
+                        emit(SearchUiState.Error("Error:${ex.message ?: "Unknown Error"}"))
+                    }
+                }
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000L),
+                initialValue = SearchUiState.Empty
+            )
 
     fun onSearchQueryChange(newSearchQuery: String) {
         _searchQuery.value = newSearchQuery
-    }
-
-    fun handleResult(cities: List<City>) {
-        if (cities.isEmpty()) {
-            genericError()
-        } else {
-            _searchUiState.value = SearchUiState.Success(cities)
-        }
-    }
-
-    fun handleException(ex: Exception) {
-        genericError()
-        Log.e(SearchViewModel::class.java.name, "onSearchQueryChange, ${ex.message}")
-    }
-
-    private fun genericError() {
-        _searchUiState.value = SearchUiState.Error("No matching results...")
     }
 
     sealed interface SearchUiState {
